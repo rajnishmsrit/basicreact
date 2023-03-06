a7286408c22e0c27c01a6b39349ce288bb36b963
Rajnish Kumar <rajnishmsrit@gmail.com>
2023-03-06 08:45:16 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: 'build',
-    sourcemap: true,
-    commonjsOptions: {
-      include: [],
-    },
-  },
-  optimizeDeps: {
-    disabled: false,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: '@testing-library/jest-dom',
-    mockReset: true,
-  },
-});
+})
